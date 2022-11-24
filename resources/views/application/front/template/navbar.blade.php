d916dcfc22886f763d9e6805121b3e37a500a081
Douglas Men <douglas.men@smplaces.com.br>
2022-11-24 20:08:20 -0300

    Logo redirecinando para home

--- resources/views/application/front/template/navbar.blade.php
+++ resources/views/application/front/template/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
             <img src="{{asset('img/logo-apostador.png')}}" width="160" alt="">
           </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
