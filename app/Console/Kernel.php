0f336df5c65543222f4716786b84cd687b3b1f13
Douglas Men <douglas.men@smplaces.com.br>
2022-12-01 13:03:31 -0300

    Agendando atualização de ciclos semanalmente

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('update-loto-results')->daily();
+        $schedule->command('update-loto-cycles')->weekly();
     }
 
     /**
