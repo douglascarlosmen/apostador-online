071bb71f4d2800c8f726c84de2e5401396668fb8
Douglas Men <douglas.men@smplaces.com.br>
2022-12-01 17:34:43 -0300

    Endpoint para Conferir acertos de outros jogos

--- app/Services/ConferidorService.php
+++ app/Services/ConferidorService.php
@@ -2,10 +2,35 @@
 
 namespace App\Services;
 
+use App\Models\Loto;
+
 class ConferidorService
 {
     public function checkNumberOfHits(array $dozensBet, array $resultDozens)
     {
         return count(array_intersect($dozensBet, $resultDozens));
+    }
+
+    public function getOtherPrimesWithDozens(string $loto, array $dozens, int|string $minElegible)
+    {
+        $loto = Loto::whereName($loto)->with('results')->first();
+        $othersResults = [];
+
+        foreach ($loto->results as $result) {
+            $resultDozensArray = json_decode($result->dozens, true);
+            $assertsCount = count(array_intersect($dozens, $resultDozensArray));
+
+            if($assertsCount >= $minElegible) {
+                if (!isset($othersResults["$assertsCount"])) {
+                    $othersResults["$assertsCount"]['count'] = 1;
+                } else {
+                    $othersResults["$assertsCount"] += 1;
+                }
+                $othersResults["$assertsCount"]['contests'][] = $result->contest_number;
+            }
+        }
+
+        return $othersResults;
+
     }
 }
