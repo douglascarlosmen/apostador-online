2134a3c779b9bcbe96d0f17c64a012c248dc9c9e
Kaio <kaa.borges10@hotmail.com>
2023-02-03 16:47:18 -0300

    layout quina

---
+++ resources/views/application/tickets/quina/game.blade.php
@@ -0,0 +1,271 @@
+<div class="ticket-body">
+    <div class="column">
+        <div class="ticket-dozen" id="01">
+            @if(str_contains($game, "01")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="11">
+            @if(str_contains($game, "11")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="21">
+            @if(str_contains($game, "21")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="31">
+            @if(str_contains($game, "31")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="41">
+            @if(str_contains($game, "41")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="51">
+            @if(str_contains($game, "51")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="61">
+            @if(str_contains($game, "61")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="71">
+            @if(str_contains($game, "71")) <div class="{{$type}}"></div> @endif
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="ticket-dozen" id="02">
+            @if(str_contains($game, "02")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="12">
+            @if(str_contains($game, "12")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="22">
+            @if(str_contains($game, "22")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="32">
+            @if(str_contains($game, "32")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="42">
+            @if(str_contains($game, "42")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="52">
+            @if(str_contains($game, "52")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="62">
+            @if(str_contains($game, "62")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="72">
+            @if(str_contains($game, "72")) <div class="{{$type}}"></div> @endif
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="ticket-dozen" id="03">
+            @if(str_contains($game, "03")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="13">
+            @if(str_contains($game, "13")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="23">
+            @if(str_contains($game, "23")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="33">
+            @if(str_contains($game, "33")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="43">
+            @if(str_contains($game, "43")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="53">
+            @if(str_contains($game, "53")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="63">
+            @if(str_contains($game, "63")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="73">
+            @if(str_contains($game, "73")) <div class="{{$type}}"></div> @endif
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="ticket-dozen" id="04">
+            @if(str_contains($game, "04")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="14">
+            @if(str_contains($game, "14")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="24">
+            @if(str_contains($game, "24")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="34">
+            @if(str_contains($game, "34")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="44">
+            @if(str_contains($game, "44")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="54">
+            @if(str_contains($game, "54")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="64">
+            @if(str_contains($game, "64")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="74">
+            @if(str_contains($game, "74")) <div class="{{$type}}"></div> @endif
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="ticket-dozen" id="05">
+            @if(str_contains($game, "05")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="15">
+            @if(str_contains($game, "15")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="25">
+            @if(str_contains($game, "25")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="35">
+            @if(str_contains($game, "35")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="45">
+            @if(str_contains($game, "45")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="55">
+            @if(str_contains($game, "55")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="65">
+            @if(str_contains($game, "65")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="75">
+            @if(str_contains($game, "75")) <div class="{{$type}}"></div> @endif
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="ticket-dozen" id="06">
+            @if(str_contains($game, "06")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="16">
+            @if(str_contains($game, "16")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="26">
+            @if(str_contains($game, "26")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="36">
+            @if(str_contains($game, "36")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="46">
+            @if(str_contains($game, "46")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="56">
+            @if(str_contains($game, "56")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="66">
+            @if(str_contains($game, "66")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="76">
+            @if(str_contains($game, "76")) <div class="{{$type}}"></div> @endif
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="ticket-dozen" id="07">
+            @if(str_contains($game, "07")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="17">
+            @if(str_contains($game, "17")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="27">
+            @if(str_contains($game, "27")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="37">
+            @if(str_contains($game, "37")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="47">
+            @if(str_contains($game, "47")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="57">
+            @if(str_contains($game, "57")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="67">
+            @if(str_contains($game, "67")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="77">
+            @if(str_contains($game, "77")) <div class="{{$type}}"></div> @endif
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="ticket-dozen" id="08">
+            @if(str_contains($game, "08")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="18">
+            @if(str_contains($game, "18")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="28">
+            @if(str_contains($game, "28")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="38">
+            @if(str_contains($game, "38")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="48">
+            @if(str_contains($game, "48")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="58">
+            @if(str_contains($game, "58")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="68">
+            @if(str_contains($game, "68")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="78">
+            @if(str_contains($game, "78")) <div class="{{$type}}"></div> @endif
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="ticket-dozen" id="09">
+            @if(str_contains($game, "09")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="19">
+            @if(str_contains($game, "19")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="29">
+            @if(str_contains($game, "29")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="39">
+            @if(str_contains($game, "39")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="49">
+            @if(str_contains($game, "49")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="59">
+            @if(str_contains($game, "59")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="69">
+            @if(str_contains($game, "69")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="79">
+            @if(str_contains($game, "79")) <div class="{{$type}}"></div> @endif
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="ticket-dozen" id="10">
+            @if(str_contains($game, "10")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="20">
+            @if(str_contains($game, "20")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="30">
+            @if(str_contains($game, "30")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="40">
+            @if(str_contains($game, "40")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="50">
+            @if(str_contains($game, "50")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="60">
+            @if(str_contains($game, "60")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="70">
+            @if(str_contains($game, "70")) <div class="{{$type}}"></div> @endif
+        </div>
+        <div class="ticket-dozen" id="80">
+            @if(str_contains($game, "80")) <div class="{{$type}}"></div> @endif
+        </div>
+    </div>
+</div>
